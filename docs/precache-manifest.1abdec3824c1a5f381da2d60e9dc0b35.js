cf9e9cf28c546c0d25ae22796ae408ee6bd7fe40
Ivan Fernandez Gracia <48660555+IvanFernandezGracia@users.noreply.github.com>
2021-11-08 22:52:09 -0300

    Second commit

---
+++ docs/precache-manifest.1abdec3824c1a5f381da2d60e9dc0b35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71778b355769a427ecc1f60e39b490d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd1173b176e3fdde57fc",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1173b176e3fdde57fc",
+    "url": "/static/js/main.f37d5e34.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
